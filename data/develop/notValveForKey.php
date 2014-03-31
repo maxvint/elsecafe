cd0b366cd87db589294b1ec556be7a3e0119fc8e
yuwenhui <yuwenhui1986@163.com>
2014-03-31 17:08:50 +0800

    first commit

---
+++ data/develop/notValveForKey.php
@@ -0,0 +1,7 @@
+<?php
+return array (
+  'PUBLIC_GET_USERINFO_FAIL' => '?app=public&mod=Passport&act=login',
+  'PUBLIC_LOGIN_AUTOMATICALLY' => '?app=public&mod=Passport&act=login',
+  'PUBLIC_REMOTE_APP' => '?app=public&mod=Index&act=index',
+);
+?>
---
+++ data/iswaf/config.php
@@ -0,0 +1,18 @@
+<?php
+return array (
+  'iswaf_database' => 'G:/xampp/htdocs/elsecafe//data/iswaf/',
+  'iswaf_connenct_key' => '9fea39b9bbed08989731ee6e6e15228a',
+  'iswaf_status' => 1,
+  'defences' => 
+  array (
+    'callback_xss' => 'On',
+    'upload' => 'On',
+    'inject' => 'On',
+    'filemode' => 'On',
+    'webshell' => 'On',
+    'server_args' => 'On',
+    'webserver' => 'On',
+    'hotfixs' => 'On',
+  ),
+);
+?>
